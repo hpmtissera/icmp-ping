e35bec8e5fcc046c19d94a61dd8fcb5f27396ed1
Prasad <prasad@lvi.co.jp>
2019-02-12 19:19:52 +0900

    working

--- main.c
+++ main.c
@@ -6,10 +6,9 @@
 #include <stdio.h>
 #include <arpa/inet.h>
 
-main()
+int main()
 {
     char *message = "hello5545";
-    int dest_port = 5000;
     struct sockaddr_in6 dest_addr; //set up dest address info
 
     int sockfd = socket(PF_INET6, SOCK_RAW, IPPROTO_ICMP); //create socket
@@ -17,10 +16,12 @@ main()
     dest_addr.sin6_family = AF_INET6;
     inet_pton (AF_INET6, "::1",  &dest_addr.sin6_addr);
 
+//    char str[INET6_ADDRSTRLEN];
+//    inet_ntop(AF_INET6, &(dest_addr.sin6_addr), str, INET6_ADDRSTRLEN);
+//    printf("%s\n", str);
+
     long out = sendto (sockfd, message, strlen(message)+1, 0 , (struct sockaddr_in6 *)&dest_addr, sizeof(struct sockaddr_in6));
     printf("output %ld\n", out);
-}
 
-// now get it back and print it
-//    inet_ntop(AF_INET6, &(sa.sin6_addr), str, INET6_ADDRSTRLEN);
-//    printf("%s\n", str); // prints "2001:db8:8714:3a90::12"
+    return 0;
+}
