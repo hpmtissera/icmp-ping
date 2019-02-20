de9a5c4ebdb1782d9ca3e4a721f953235c0b9de6
Prasad <hpmtissera@gmail.com>
2019-02-20 17:04:37 +0900

    before change header to icmp6

--- main.c
+++ main.c
@@ -3,15 +3,14 @@
 #include <arpa/inet.h>
 #include <net/if.h>
 
-void hexDump(char *desc, void *addr, int len)
-{
+void hexDump(char *desc, void *addr, int len) {
     int i;
     unsigned char buff[17];
-    unsigned char *pc = (unsigned char*)addr;
+    unsigned char *pc = (unsigned char *) addr;
 
     // Output description if given.
     if (desc != NULL)
-        printf ("%s:\n", desc);
+        printf("%s:\n", desc);
 
     // Process every byte in the data.
     for (i = 0; i < len; i++) {
@@ -68,6 +67,23 @@ int main() {
         } un;
     };
 
+    struct icmp6_hdr {
+        u_int8_t icmp6_type;    /* type field */
+        u_int8_t icmp6_code;    /* code field */
+        u_int16_t icmp6_cksum;    /* checksum field */
+        union {
+            u_int32_t icmp6_un_data32[1]; /* type-specific field */
+            u_int16_t icmp6_un_data16[2]; /* type-specific field */
+            u_int8_t icmp6_un_data8[4];  /* type-specific field */
+        } icmp6_dataun;
+    } __attribute__((__packed__));
+
+    struct icmp6_nodeinfo {
+        struct icmp6_hdr icmp6_ni_hdr;
+        u_int8_t icmp6_ni_nonce[8];
+        /* could be followed by reply data */
+    }__attribute__((__packed__));
+
     struct sockaddr_in6 ipv6_addr; //set up dest address info
     struct sockaddr_in ipv4_addr;
 
@@ -102,8 +118,8 @@ int main() {
 
     printf("Interface id : %d\n", if_nametoindex("en0"));
 
-//    inet_pton(AF_INET6, "::1", &ipv6_addr.sin6_addr);
-    inet_pton(AF_INET6, "2001:4860:4860::8888", &ipv6_addr.sin6_addr);
+    inet_pton(AF_INET6, "::1", &ipv6_addr.sin6_addr);
+//    inet_pton(AF_INET6, "2001:4860:4860::8888", &ipv6_addr.sin6_addr);
 
 
 //    long ipv4_icmp = sendto(sockfd_v4, data, sizeof icmp_hdr + 7, 0, (struct sockaddr_in *) &ipv4_addr,
@@ -112,10 +128,50 @@ int main() {
 //
     long ipv6_icmp = sendto(sockfd_v6, data_v6, sizeof icmp_hdr6 + 7, 0, (struct sockaddr_in6 *) &ipv6_addr,
                             sizeof(struct sockaddr_in6));
-    printf("output ipv6_icmp %ld\n", ipv6_icmp);
+    printf("output ipv6_icmp : %ld\n", ipv6_icmp);
+
+    struct msghdr m;
+    struct iovec iov[2];
+    struct cmsghdr *cm = NULL;
+
+# define CONTROLLEN    10240
+
+    m.msg_name = (caddr_t) &ipv6_addr;
+    m.msg_namelen = sizeof(ipv6_addr);
+    memset(&iov, 0, sizeof(iov));
+    iov[0].iov_base = (caddr_t) data_v6;
+    iov[0].iov_len = sizeof(data_v6);
+    m.msg_iov = iov;
+    m.msg_iovlen = 1;
+
+//    memset(cm, 0, CONTROLLEN);
+
+    m.msg_control = (void *) cm;
+//    m.msg_controllen = CONTROLLEN;
+
+    long cc = recvmsg(sockfd_v6, &m, 0);
+    printf("cc : %ld\n", cc);
+
+    struct sockaddr *from;
+    int fromlen;
+    struct icmp6_hdr *icp;
+    struct icmp6_nodeinfo *ni;
+    u_int16_t seq;
+
+    if (cc > 0) {
+        from = (struct sockaddr *) (&m)->msg_name;
+        fromlen = (&m)->msg_namelen;
+        icp = (struct icmp6_hdr *) data_v6;
+        ni = (struct icmp6_nodeinfo *) data_v6;
+        seq = ntohs(*(u_int16_t *)ni->icmp6_ni_nonce);
+
+        printf("Icmp6 type : %d\n", icp->icmp6_type);
+        printf("Icmp6 seq : %d\n", seq);
+
+    }
 
 //    int sockfd_v4_raw = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP); //create socket
-    int sockfd_v6_raw = socket(AF_INET6, SOCK_RAW, IPPROTO_ICMPV6); //create socket
+//    int sockfd_v6_raw = socket(AF_INET6, SOCK_RAW, IPPROTO_ICMPV6); //create socket
 
 //    int bindResult = bind(sockfd_v6_raw, (struct sockaddr *)&ipv6_addr, sizeof(ipv6_addr));
 //    printf("bind result v6-raw : %d\n", bindResult);
@@ -130,9 +186,9 @@ int main() {
 
 //    hexDump(NULL, &ipv6_addr, sizeof(ipv6_addr));
 //
-    long ipv6_raw = sendto(sockfd_v6_raw, data_v6, sizeof icmp_hdr6 + 7, 0, (struct sockaddr_in6 *) &ipv6_addr,
-                           sizeof(struct sockaddr_in6));
-    printf("output ipv6_raw %ld\n", ipv6_raw);
+//    long ipv6_raw = sendto(sockfd_v6_raw, data_v6, sizeof icmp_hdr6 + 7, 0, (struct sockaddr_in6 *) &ipv6_addr,
+//                           sizeof(struct sockaddr_in6));
+//    printf("output ipv6_raw %ld\n", ipv6_raw);
 
     return 0;
 }
